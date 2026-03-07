d311de650d01ede5c62da2e92e3630fdf8fc03f5
Kevin-Salazar-D <kevinyahirsalazar342@gmail.com>
2026-03-07 17:48:51 -0600

    agregnado endpoitnts usuario

--- controllers/userController/index.js
+++ controllers/userController/index.js
@@ -50,7 +50,7 @@ const userController = (servicioUsuario) => {
             const resultado = await servicioUsuario.buscarUsuarioPorId(id);
             res.status(200).json({message: "Usuario encontrado", usuario: resultado});
         }catch(error){
-            res.status(500).json({error: error.message});
+            res.status(error.status|| 500).json({error: error.message});
         }
     };
 
@@ -60,7 +60,7 @@ const userController = (servicioUsuario) => {
             const resultado = await servicioUsuario.buscarUsuarioPorCorreo(correo);
             res.status(200).json({message: "Usuario encontrado", usuario: resultado});
         }catch(error){
-            res.status(500).json({error: error.message});
+            res.status(error.status|| 500).json({error: error.message});
         }
     }
 
