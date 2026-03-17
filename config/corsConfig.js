5ca45ab714d5195d991cbe731bfbcc2c2dbec723
Kevin-Salazar-D <kevinyahirsalazar342@gmail.com>
2026-03-17 11:42:27 -0600

    fix: Agregando url de produccion del front

--- config/corsConfig.js
+++ config/corsConfig.js
@@ -1,7 +1,7 @@
 import cors from "cors";
-
+const urlProduccion = process.env.FRONTEND_URL;
 const corsInfo = {
-  origin: ['http://localhost:5173', 'http://192.168.100.15:5173'],
+  origin: ['http://localhost:5173', 'http://192.168.100.15:5173', urlProduccion],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   //autorizar por falta de token
   allowedHeaders: ["Content-Type", "Authorization" ],
