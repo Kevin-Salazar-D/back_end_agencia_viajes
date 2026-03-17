5a6b2d286fe63c2e8da393d7001ba4504a322c6a
Kevin-Salazar-D <kevinyahirsalazar342@gmail.com>
2026-03-17 12:48:17 -0600

    bugfix: Arreglando endpont logut y subiendo version

--- config/swaggerConfig.js
+++ config/swaggerConfig.js
@@ -6,7 +6,7 @@ const swaggerOptions = {
     openapi: "3.0.0",
     info: {
       title: "API de agencia de viajes",
-      version: "3.6.1",
+      version: "3.7.1",
       description: "Gestiona usuarios, ciudades y hoteles",
     },
     servers: [
